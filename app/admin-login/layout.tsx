20cf59138a12962c886dbb881c3842bbd8560035
AirsoftSystem <airsoftsystemsiege@gmail.com>
2026-06-01 12:06:38 +0200

    13ème Nouvelle page messagerie

--- app/admin-login/layout.tsx
+++ app/admin-login/layout.tsx
@@ -1,6 +1,6 @@
 
 /**
- * Layout spécifique pour la page de login admin
+ * Layout vide pour la page de connexion admin
  * Aucun menu latéral, pleine page
  */
 export default function AdminLoginLayout({
